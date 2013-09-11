8242634eafbc3f7ca8a5989210f1a5790466c32f
Nik Daftary <nikdaftary@gmail.com>
2013-09-11 14:59:53 -0500

    adding passing tests, eliminated failing test.  Checking for literal true value for key:vegetarian

--- pizza.rb
+++ pizza.rb
@@ -4,7 +4,17 @@ module Pizza
   end
 
   class Topping
-  	
+  	attr_accessor :name, :vegetarian
+  # Instantiate a new topping.
+  #
+  # name - The String name of the topping.
+  #
+  # Returns a new Topping object.
+
+  	def initialize(name, vegetarian: false)
+  		@name = name
+  		@vegetarian = vegetarian
+  	end
   end
 end
 
--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -9,13 +9,20 @@ end
 
 describe Pizza::Topping do #in the Pizza module, Topping Class, do the following:
 	
-
-
 	describe '.initalize' do
 	# In RSpec, we use describe blocks to specify what we are testing. At the outer level, we describe the Pizza::Topping class, and within that, we describe the initialize method. The . indicates that this is a class method (since it is called directly off of the Pizza::Topping class: Pizza::Topping.new).
+
+		it "sets whether or not the topping is vegetarian" do
+			topping = Pizza::Topping.new('bell peppers', vegetarian: true)
+			
+			expect(topping.vegetarian).to be_true # this checks for literal truthyness
+		end
+
+
+
 		it "sets the name of the topping" do
 			#The first line we added is essentially the same code we would run in irb - we create a new Topping and pass the string 'olives' to set the @name. The second line is our expectation: we expect that topping.name will be equal to 'olives'.
-			topping = Pizza::Topping.new('olives')
+			topping = Pizza::Topping.new('olives', vegetarian: true)
 
 			expect(topping.name).to eq('olives')
 		end
@@ -28,3 +35,4 @@ describe Pizza::Topping do #in the Pizza module, Topping Class, do the following
 end
 
 # to run this script: bundle exec rspec spec/pizza_spec.rb
+# run this within the root directory
